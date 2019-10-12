ba0cd756282dc39588c369ba90c6bab03313bc83
Koala <bozonedouard@gmail.com>
2019-10-12 14:21:05 +0200

    Add http and socket io server

---
+++ projects/api/index.js
@@ -0,0 +1,17 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('a user disconnected');
+  });
+});
+
+http.listen(process.env.PORT || 3000);
