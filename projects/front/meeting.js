994096a9225fa092a4a976ab9228fc7286bd76a3
Koala <bozonedouard@gmail.com>
2019-10-12 20:52:20 +0200

    Improve web app

--- projects/front/meeting.js
+++ projects/front/meeting.js
@@ -1,7 +1,4 @@
 export default {
-  total: 0,
-  current: 0,
-  width: 300,
   join({name, salary}) {
     axios
       .post("/meeting/join", {
@@ -11,33 +8,5 @@ export default {
   },
   start() {
     axios.post("/meeting/start");
-  },
-  init() {
-    const socket = io();
-    socket.on("meeting:total-price", price => {
-      console.log(price)
-      document.getElementById("current-price").innerText = price.toLocaleString(
-        "fr-FR",
-        { style: "currency", currency: "EUR" },
-      );
-    }),
-      (this.total = 180);
-    document.getElementById("init-screen").style.display = "none";
-    document.getElementById("progress-screen").style.display = "block";
-    const interval = setInterval(() => {
-      this.current += 1;
-      if (this.current <= this.total) {
-        document.getElementById(
-          "progress-bar",
-        ).style.width = this.getCurrentWidth();
-      } else {
-        document.getElementById("progress-screen").style.display = "none";
-        document.getElementById("game-over-screen").style.display = "block";
-        clearInterval(interval);
-      }
-    }, 1000);
-  },
-  getCurrentWidth() {
-    return `${(this.width * this.current) / this.total}px`;
   },
 };
