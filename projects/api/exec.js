75a8261c65c9e3ff99a76d91d0ddf0f5c4ef1059
Koala <bozonedouard@gmail.com>
2019-10-12 17:12:31 +0200

    Add fire endpoint

--- projects/api/exec.js
+++
@@ -1,8 +0,0 @@
-const { exec } = require('child_process');
-exec(__dirname + '/bin/missilelauncher', (error, stdout, stderr) => {
-  if (error) {
-    console.log(error);
-    throw error;
-  }
-  console.log(stdout);
-});
--- projects/api/index.js
+++ projects/api/index.js
@@ -1,46 +1,45 @@
 const app = require("express")();
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
-const { spawn } = require('child_process');
+const { execSync } = require("child_process");
 
-const { Person, Meeting } = require('./model');
+const { Person, Meeting } = require("./model");
+
+const missileLauncher = command =>
+  execSync("sudo " + __dirname + "/bin/missilelauncher " + command);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const currentMeeting = new Meeting({
-  duration: 1000 * 60 * 3 // 3m
+  duration: 1000 * 60 * 3, // 3m
 });
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.post('/meeting/join', (req, res) => {
+app.post("/meeting/join", (req, res) => {
   const person = new Person({ ...req.body });
   currentMeeting.addPerson(person);
-  io.emit('meeting:person-joined', person);
+  io.emit("meeting:person-joined", person);
   res.status(200).end();
 });
 
-app.post('/meeting/start', (req, res) => {
+app.post("/meeting/start", (req, res) => {
   currentMeeting.start();
-  io.emit('meeting:started', currentMeeting.startedAt);
+  io.emit("meeting:started", currentMeeting.startedAt);
   res.status(200).end();
 });
 
-app.post('/fire', (req, res) => {
-  const process = spawn(__dirname + '/bin/missilelauncher');
-  process.on('')
-  process.on('data', data => console.log(data));
-  process.on('error', () => res.status(500));
-  process.on('close', () => res.status(200));
-
+app.post("/fire", (req, res) => {
+  missileLauncher('fire');
+  res.end();
 });
 
 io.on("connection", socket => {
-  socket.emit('meeting', currentMeeting);
+  socket.emit("meeting", currentMeeting);
 });
 
 http.listen(process.env.PORT || 3000);
