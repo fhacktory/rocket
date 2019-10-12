f77f3cb8950e1dcd164fc227bb22316a806e28b7
Koala <bozonedouard@gmail.com>
2019-10-12 15:20:27 +0200

    Add model

--- projects/api/index.js
+++ projects/api/index.js
@@ -1,16 +1,55 @@
-const app = require('express')();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const app = require("express")();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+// 200 = working days per year
+// 7 = working hours per day
+const getPricePerSecond = salary => salary / 200 / 7 / 60 / 60;
+
+class Person {
+  constructor({ name, salary }) {
+    this.name = name;
+    this.salary = salary;
+  }
+
+  getPrice(meeting) {
+    const now = new Date().getTime();
+    const elapsed = now - meeting.startedAt.getTime();
+    return getPricePerSecond(this.salary) * (elapsed / 1000);
+  }
+}
+
+class Meeting {
+  constructor({ duration, persons }) {
+    this.startedAt = null;
+    this.duration = duration;
+    this.persons = persons;
+  }
+
+  start() {
+    this.startedAt = new Date();
+  }
+
+  getTotalPrice() {
+    return this.persons.reduce((acc, person) => person.getPrice(this) + acc, 0);
+  }
+}
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
+io.on("connection", socket => {
+  const person = new Person({ id: 0, name: "Chirac", salary: 60000 });
+  const currentMeeting = new Meeting({
+    duration: 1000 * 60 * 60, // 1h
+    persons: [person],
+  });
 
-  socket.on('disconnect', () => {
-    console.log('a user disconnected');
+  console.log(currentMeeting);
+
+  socket.on("disconnect", () => {
+    console.log("a user disconnected");
   });
 });
 
