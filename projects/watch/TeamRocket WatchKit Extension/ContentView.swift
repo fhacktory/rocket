27abe58bf7c42a831e340709f2527064126ca271
NicolasLebrun <nicolas.lebrun@worldline.com>
2019-10-12 20:53:05 +0200

    Rework on watch face UI

--- projects/watch/TeamRocket WatchKit Extension/ContentView.swift
+++ projects/watch/TeamRocket WatchKit Extension/ContentView.swift
@@ -11,23 +11,34 @@ import Starscream
 
 struct ContentView: View {
     @EnvironmentObject var moneyCounter: MoneyCounter
+    @State var timerStarted: Bool = false
 
     var body: some View {
-        List {
-            Text("💸 \(moneyCounter.moneyDescription) €")
+        VStack {
+            Text("Meeting Cost").font(.system(size:30))
+            HStack {
+                Text("\(moneyCounter.moneyDescription) €").bold().font(.system(size: 25))
+                Spacer()
+                Text("💸")
+            }.padding()
+            if !timerStarted {
+                Button(action: {
+                    self.moneyCounter.startTimer()
+                    self.timerStarted = true
+                }, label: {
+                    HStack {
+                        Text("Start").foregroundColor(Color.blue).font(.system(size:20))
+                        Spacer()
+                        Image(systemName: "play").imageScale(.large).foregroundColor(Color.blue)
+                    }.padding()
+                })
+            }
             if moneyCounter.money > 300.0 {
                 Button(action: {
                     print("Fired !")
                 }, label: {
                     Text("💥")
-                    .font(.system(size: 100))
-                })
-            } else {
-                Button(action: {
-                    self.moneyCounter.startTimer()
-                }, label: {
-                    Text("🚀")
-                    .font(.system(size: 40))
+                    .font(.system(size: 50))
                 })
             }
         }
